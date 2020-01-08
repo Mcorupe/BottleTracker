7f458df36c587716613cf0b5e92a73ad85cb282a
mark <markcorupe@gmail.com>
2020-01-08 14:55:32 -0700

    Jest config

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ function Layout({ children }) {
         >
           {children}
           <button
-            className="rounded-full p-2 px-3 bg-teal-700 text-white hover:bg-teal-600 "
+            className="rounded-full p-2 mx-64 bg-teal-700 text-white hover:bg-teal-600 "
             type="button"
             onClick={handleTheme}
           >
@@ -33,7 +33,7 @@ function Layout({ children }) {
           <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
             <p className="text-white">
               Created by{" "}
-              <a className="font-bold no-underline text-white" title="Hello">
+              <a className="font-bold no-underline text-white" title="Why hello there">
                 Kevin and Mark
               </a>
             </p>
